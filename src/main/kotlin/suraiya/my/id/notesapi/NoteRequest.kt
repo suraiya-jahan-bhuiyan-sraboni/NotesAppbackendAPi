40c0944482ba35f0492f84f2ff4d7f3d6f217fee
suraiya-jahan-bhuiyan-sraboni <suraiya.jahan.bhuiyan.12@gmail.com>
2024-02-19 19:32:48 +0600

    docker added

--- src/main/kotlin/suraiya/my/id/notesapi/NoteRequest.kt
+++
@@ -1,4 +0,0 @@
-package suraiya.my.id.notesapi
-
-data class NoteRequest(val title : String, val description : String, val userId : Int)
-
--- src/main/kotlin/suraiya/my/id/notesapi/NoteResponse.kt
+++
@@ -1,3 +0,0 @@
-package suraiya.my.id.notesapi
-
-data class NoteResponse(val id : Int, val title : String, val description : String)
